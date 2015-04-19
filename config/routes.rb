137e4b5c71c9b42c4d7a8a7721c1e828f13456f7
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-18 19:26:20 -0600

    styled login and register form

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  resources :users
+
 end
--- spec/features/user_register_spec.rb
+++ spec/features/user_register_spec.rb
@@ -4,7 +4,7 @@ describe "User Register", :type => :feature do
   it "can go to register page" do
     visit root_path
     click_link_or_button("register")
-    expect(page).to have_content("Waterspot")
+    expect(page).to have_content("waterspot")
     expect(page).to have_content("login")
   end
 end
