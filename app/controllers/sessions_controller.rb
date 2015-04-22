f06691e08449394904e5eaebf9fd8d110f634b12
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-22 10:03:03 -0600

    Rendered error when unregistered user tries to login with twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     if params[:provider]
-      user = User.find_or_create_by_auth(request.env['omniauth.auth'])
-      if user
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        redirect_to root_path
-      end
+      oauth_login
     else
-      user = User.find_by(email: params[:session][:email])
-      if user
-        session[:user_id] = user.id
-        redirect_to user_path(user)
-      else
-        render :new
-      end
+      user_login
     end
   end
 
@@ -26,5 +14,28 @@ class SessionsController < ApplicationController
     session.clear
     flash[:notice] = "you have been signed out"
     redirect_to root_path
+  end
+
+  private
+
+  def oauth_login
+    user = User.find_by_auth(request.env['omniauth.auth'])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Please register with waterspot"
+      redirect_to root_path
+    end
+  end
+
+  def user_login
+    user = User.find_by(email: params[:session][:email])
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
   end
 end
