7949c8ac96c088b9003193f99b34a03ed73a66d8
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-22 14:53:35 -0600

    Fixed twitter login to work with app registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
   has_secure_password
   has_many :spots
 
   mount_uploader :avatar, AvatarUploader
 
-  def self.find_by_auth(data)
-    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+  def self.find_or_create_by_auth(data)
+    user = User.find_or_create_by(username: data.info.nickname )
     if user
-      user.username   = data.info.nickname
       user.avatar     = data.info.image
       user.token      = data.credentials.token
+      user.provider   = data.provider
+      user.uid        = data.uid
       user.save
       return user
     end
