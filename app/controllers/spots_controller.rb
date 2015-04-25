14fffb2bd1b64baa49854ca1a606fe59988345b0
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-25 17:59:45 -0600

    troubleshooting heroku

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,33 +4,29 @@ class SpotsController < ApplicationController
     @spots = Spot.all
     @geojson = Array.new
 
-    if current_user
-      @spots.each do |spot|
-        @geojson << {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [spot.long.to_f, spot.lat.to_f]
-          },
-          properties: {
-            name:   spot.user.username,
-            date:   spot.formatted_date,
-            rating: spot.rating,
-            :'marker-color' => '#00607d',
-            :'marker-symbol' => 'circle',
-            :'marker-size' => 'small'
-          }
+    @spots.each do |spot|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [spot.long.to_f, spot.lat.to_f]
+        },
+        properties: {
+          name:   spot.user.username,
+          date:   spot.formatted_date,
+          rating: spot.rating,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'small'
         }
-      end
-
-      respond_to do |format|
-        format.html
-        format.json { render json: @geojson }
-      end
-    else
-      redirect_to root_path
-      @spots = nil
+      }
     end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson }
+    end
+
   end
 
   private
