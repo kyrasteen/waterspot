1cea168a420de37ecd188cbbd3b7259827207be2
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-25 11:11:08 -0600

    Email preview working

--- spec/mailers/previews/user_notifier_preview.rb
+++ spec/mailers/previews/user_notifier_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_notifier
 class UserNotifierPreview < ActionMailer::Preview
-
+  def send_signup_email_preview
+    UserNotifier.send_signup_email(User.last)
+  end
 end
