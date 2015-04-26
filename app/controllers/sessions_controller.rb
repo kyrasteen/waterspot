df61244901e83512594fd69afb655146c7b87f77
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-26 08:54:53 -0600

    pushing logic down the stack

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,7 +34,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to root_path
     else
       flash[:error]="invalid information"
       render :new
