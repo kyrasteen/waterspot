acf42df19607cd75f25ab46c37ad4417b283ce01
Kyra Weber <kyra.m.weber@gmail.com>
2015-05-02 10:14:21 -0600

    trying to fix map performance

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -2,12 +2,11 @@ class Api::V1::SpotsController < ApplicationController
   respond_to :json
 
   def index
-    if current_user
-      #cache here
+    if current_user.user.is_a?(Guest)
+      redirect_to root_path
+    else
       geojson = GeoSpots.create(Spot.all)
       respond_with geojson
-    else
-      redirect_to root_path
     end
   end
 
