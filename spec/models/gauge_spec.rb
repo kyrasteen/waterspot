1bcc1ab34e75b9b738607f1544a19b233d217daf
Kyra Weber <kyra.m.weber@gmail.com>
2015-05-02 18:09:44 -0600

    updated gauge model test

--- spec/models/gauge_spec.rb
+++ spec/models/gauge_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 describe "Gauge", type: :model do
   it "has valid attributes" do
-    gauge = Gauge.create(geometry: { coordinates: [32, -88] }, properties:
-                     { name: "Clear Creek", value: "80" })
-    expect(gauge.properties["name"]).to eq("Clear Creek")
-    expect(gauge.properties["value"]).to eq("80")
-    expect(gauge.geometry["coordinates"]).to eq("[32, -88]")
-
+    gauge = Gauge.create(lat: 32, long: -88.4, name:
+                         "Clear Creek", value: "80")
+    expect(gauge.name).to eq("Clear Creek")
+    expect(gauge.value).to eq("80")
+    expect(gauge.lat).to eq(32)
+    expect(gauge.long).to eq(-88.4)
   end
 end
