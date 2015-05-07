a81e947fa838ce44815deb73237e7ed1d3f7fc8a
Kyra Weber <kyra.m.weber@gmail.com>
2015-05-07 00:03:19 -0600

    fixed bug in area_wathes

--- app/controllers/api/v1/area_watches_controller.rb
+++ app/controllers/api/v1/area_watches_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::AreaWatchesController < ApplicationController
   def create
-    update_polygon_user_of_spot(params[:data_poly]["properties"]["user_id"])
+    user = JSON.parse(params[:data_poly])["properties"]["user_id"]
+    update_polygon_user_of_spot(user)
     head :ok
   end
 
   private
 
-  def update_polygon_user_of_spot(params)
-    spot    = current_user.spots.last
-    user    = User.find(JSON.parse(params))
+  def update_polygon_user_of_spot(user)
+    spot = current_user.spots.last
+    user = User.find(user)
     UserAreaWatch.send_update_email(user, spot).deliver
   end
 end
